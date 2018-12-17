530a3d5d837c3ba01727db25f6dedde9c605d0eb
lvshijie <shijielyu@163.com>
2018-12-17 23:03:52 +0800

    Initial commit

---
+++ src/casia/ibasic/gitdemo/DemoList.java
@@ -0,0 +1,16 @@
+package casia.ibasic.gitdemo;
+
+import java.util.ArrayList;
+
+public class DemoList {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> list =new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		System.out.println(list);
+	}
+
+}
---
+++ src/casia/ibasic/gitdemo/Hello.java
@@ -0,0 +1,38 @@
+package casia.ibasic.gitdemo;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("=======================================");
+		System.out.println("new Hello!");
+		
+		System.out.println("=======================================");
+		System.out.println("Hello world!");
+		System.out.println("---------------------------------------");
+		System.out.println("第一次提交");
+		
+		System.out.println("---------------------------------------");
+		System.out.println("第二次提交");
+		
+		System.out.println("---------------------------------------");
+		System.out.println("第三次提交");
+		
+		System.out.println("=======================================");
+		System.out.println("Hello Beijing!");
+		
+		System.out.println("---------------------------------------");
+		System.out.println("第四次提交");
+		
+		System.out.println("=======================================");
+		System.out.println("Hello ChengDu!");
+		
+		System.out.println("=======================================");
+		System.out.println("Hello ShangHai!");
+		
+		System.out.println("---------------------------------------");
+		System.out.println("第五次提交");
+		
+		System.out.println("---------------------------------------");
+		System.out.println("第六次次提交");
+		
+	}
+}
